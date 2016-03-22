d270b71e7aee6f6adfb22a1ecb1f1306c3d93124
Michal Kordas <kordas.michal@gmail.com>
2016-03-22 23:37:40 +0100

    Provide static import code

---
+++ src/main/java/com/michalkordas/intellij/show/code/StaticImports.java
@@ -0,0 +1,8 @@
+package com.michalkordas.intellij.show.code;
+
+public class StaticImports {
+    void staticImportsOnTheFly() {
+//        singleton(1);
+//        System.out.println(ENGLISH);
+    }
+}
