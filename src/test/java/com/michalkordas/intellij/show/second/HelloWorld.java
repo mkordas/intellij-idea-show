c549dbd3a61ff492ebff77fb07ee9e41bc2533f2
Michal Kordas <kordas.michal@gmail.com>
2017-03-23 10:07:00 +0100

    Rename to "a"

--- src/test/java/com/michalkordas/intellij/show/second/HelloWorld.java
+++ src/test/java/com/michalkordas/intellij/show/second/HelloWorld.java
@@ -3,7 +3,7 @@ package com.michalkordas.intellij.show.second;
 public class HelloWorld {
     @org.junit.Test
     public void name() throws Exception {
-        String _‎ = "Hello ";
+        String a‎ = "Hello ";
 //        String _‏ = "World";
 //        String _‎‏ = " !!";
 //        System.out.println(_‎+_‏+_‎‏);
