c95ee0fe1377c5c7bd27cd62da1ccfa5a7a9e7ef
Michal Kordas <kordas.michal@gmail.com>
2017-03-23 12:45:35 +0100

    Add Java 9 class

---
+++ src/main/java/com/michalkordas/intellij/show/java9/Interface.java
@@ -0,0 +1,13 @@
+package com.michalkordas.intellij.show.java9;
+
+public interface Interface {
+
+    static void staticMethod() {
+    }
+
+    default void defaultMethod() {
+    }
+
+//    private void privateMethod() {
+//    }
+}
